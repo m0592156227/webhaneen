eb0898fe652459e93dddf60233ca26d946d7736a
m0592156227 <m0592156227@gmail.com>
2022-08-23 20:00:47 +0200

    array php

---
+++ sample.php/06-Increment&Decrement-perators/07-array/arrays.php
@@ -0,0 +1,14 @@
+<?php
+ #$student1 ="Ali";
+# $student2 ="Omar";
+
+$students =["Ali","Omar"];
+array_push($students,'ahmed');
+print_r ($students);
+
+$z = ['me','you', 'he'];
+array_push($z, 'she', 'it');
+print_r($z);
+
+
+?>
