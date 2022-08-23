5f7350d2dd850f65280847d5e5d0cb1372e28bc5
m0592156227 <m0592156227@gmail.com>
2022-08-23 19:59:53 +0200

    for loop

---
+++ sample.php/06-Increment&Decrement-perators/07-array/08-if Condition/09-switch/10-loop/for.php
@@ -0,0 +1,12 @@
+<?php
+for ($x = 0; $x <= 100; $x+=10) {
+  echo "The number is: $x <br>";
+}
+?>
+
+<?php
+$arr= ["element 1","element 2","element 3","element 4"];
+for($i=0 ;$i<count($arr); $i++){
+  echo $arr[$i];
+}
+?>
