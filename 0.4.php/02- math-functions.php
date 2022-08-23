8595f95d17d0879a3f7f49c0609ae714bd725810
m0592156227 <m0592156227@gmail.com>
2022-08-23 19:57:39 +0200

    math php

---
+++ 0.4.php/02- math-functions.php
@@ -0,0 +1,26 @@
+<?php
+// absulate  //floor ///sin cos 
+
+$var=25;
+echo sqrt($var) ."<br>";
+echo(abs(-6.7))."<br>"; 
+
+echo(min(0, 150, 30, 20, -8, -200)."<br>");  // returns -200
+echo(max(0, 150, 30, 20, -8, -200))."<br>";  // returns 150
+  
+//nearest integer
+echo(round(0.60))."<br>";  // returns 1
+echo(round(0.49))."<br>";  // returns 0
+
+echo(rand())."<br>";
+
+echo(rand(10, 100))."<br>";
+
+//https://www.w3schools.com/php/php_ref_math.asp
+//
+echo(pi())."<br>"; 
+
+#predefine M_PI
+echo  M_PI*2;
+?>
+
