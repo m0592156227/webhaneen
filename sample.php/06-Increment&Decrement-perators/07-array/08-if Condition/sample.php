af0d82539cae86b4914c48364b99b251ab69583b
m0592156227 <m0592156227@gmail.com>
2022-08-23 20:00:36 +0200

    if else

---
+++ sample.php/06-Increment&Decrement-perators/07-array/08-if Condition/sample.php
@@ -0,0 +1,28 @@
+<?php
+$t = 10;
+
+if ($t < 20) {
+  echo "Have a good day!";
+}
+?>
+
+<?php 
+$t = 10;
+
+if ($t < 20) {
+  echo "Have a good day!";
+} else {
+  echo "Have a good night!";
+}
+?>
+
+<?php
+$t = 10;
+if ($t < 10) {
+  echo "Have a good morning!";
+} elseif ($t < 20) {
+  echo "Have a good day!";
+} else {
+  echo "Have a good night!";
+}
+?>
