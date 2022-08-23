3e9ed1be0b2748371f432bb362c9c641d178f3da
m0592156227 <m0592156227@gmail.com>
2022-08-23 19:58:24 +0200

    include php

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ <style>
+    ul{
+        background-color: pink;;
+    }
+    ul li{
+        list-style: none;
+        display: inline-block;
+        padding: 20px 20px;
+    }
+ </style>   
+<ul>
+        <li>home</li>
+        <li>About</li>
+        <li>service</li>
+        <li>contact</li>
+    </ul>
+</body>
+</html>
