a883b564ecce7fc980fafc826d7f4afc0bff715a
m0592156227 <m0592156227@gmail.com>
2022-08-23 20:00:03 +0200

    foreach

---
+++ sample.php/06-Increment&Decrement-perators/07-array/08-if Condition/09-switch/10-loop/foreach.php
@@ -0,0 +1,14 @@
+<?php
+$colors = array("red", "green", "blue", "yellow");
+
+foreach ($colors as $value) 
+  echo "$value <br>";
+
+  ?>
+<?php
+$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+foreach($age as $x => $val) {
+  echo "$val<br>";
+}
+?>
