57579c1ae36084b5ea87f63120099ab5f8fa9af2
m0592156227 <m0592156227@gmail.com>
2022-08-28 07:45:13 +0200

    form

--- form/form.php
+++ form/form.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
     <?php
 include "style.php";
 	?>
@@ -11,39 +13,35 @@ include "style.php";
 </head>
 <body>
 
-<form action="action.php?"method="GET">
+<form action="hand.php"method="get" id="my Form>
   <div class="form-group">
-    <label for="exampleFormControlInput1">Email address</label>
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" name ="email" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
   <div class="form-group">
-    <label for="exampleFormControlSelect1">Example select</label>
-    <select class="form-control" id="exampleFormControlSelect1">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-    </select>
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control"name="password" id="exampleInputPassword1" placeholder="Password">
   </div>
-  <div class="form-group">
-    <label for="exampleFormControlSelect2">Example multiple select</label>
-    <select multiple class="form-control" id="exampleFormControlSelect2">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-    </select>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
   </div>
-  <div class="form-group">
-    <label for="exampleFormControlTextarea1">Example textarea</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-  </div>
-  <input type="submit"value="insert">
+  <button type="submit" class="btn btn-primary"id="save-bet">Submit</button>
 </form>
 <?php
 include "javascripts.php";
 ?>
+<script>
+  $ (document). ready(function(){
+    $ ('#save-bet'). click(function(){
+      var result =confirm('Are you sure');
+      if (result == true){
+        $('#my Form').submit();
+      }
+    });
+
+  });
+</script>
 </body>
 </html>
